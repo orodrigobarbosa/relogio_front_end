9ce1a9eec70ddfe633c90655cb34bd3b71a8c748
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-09-03 10:02:54 -0300

    Atividade Back End AAvancado

---
+++ script.js
@@ -0,0 +1,12 @@
+const hoursElement = document.querySelector('.hours');
+const minutesElement = document.querySelector('.minutes');
+const secondsElement = document.querySelector('.seconds');
+
+function atualizarHora() {
+    const now = new Date();
+    hoursElement.textContent = now.getHours().toString().padStart(2, '0');
+    minutesElement.textContent = now.getMinutes().toString().padStart(2, '0');
+    secondsElement.textContent = now.getSeconds().toString().padStart(2, '0');
+}
+
+setInterval(atualizarHora, 1000);
